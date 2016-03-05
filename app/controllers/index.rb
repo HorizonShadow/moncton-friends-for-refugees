60028520c1371439bfebff891596b8bfbb8639f6
Joshua LeBlanc <joshleblanc94@gmail.com>
2016-03-04 20:15:17 -0400

    added items we need page

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -11,9 +11,14 @@ module Site
       show_how_to_help = lambda do
         render_page :how_to_help
       end
+      
+      show_items_we_need = lambda do
+        render_page :items_we_need
+      end
 
       get '/', &show_landing
       get '/how-to-help/?', &show_how_to_help
+      get '/items-we-need/?', &show_items_we_need
     end
   end
 end
